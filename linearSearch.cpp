4268c24bc06f3c84e347bc4eee385afe51b54517
RizkyPratama05 <rizkypp428@gmail.com>
2024-04-04 21:04:34 +0700

    menambahkan algoritma linear search

--- linearSearch.cpp
+++ linearSearch.cpp
@@ -29,3 +29,37 @@ void input ()
     }
 }
 
+void LinearSearch()
+{
+    char ch;
+    int ctr; // Number of comparisons
+    int item;
+
+    do 
+    {
+        //Accept the number to be ssearched
+        cout << "\nEnter the element you to search: "; //step 1
+        cin >> item;
+        
+        ctr = 0;
+        i = 0; // step 2
+        while (i < n) // step 3
+        {
+            ctr++;
+            if (arr[i] == item)
+            {
+                cout << "\n" << item << " found at position " << (i + 1) << endl; //step 6
+                break;
+            }
+            i++; //step 4
+        }
+        if (i == n) // step 5
+            cout << "\n" << item << " not found in the array\n";
+        cout << "\nNumber of comparisons: " << endl;
+
+        cout << "\nContinue search (y/n): ";
+        cin >> ch;
+
+    } while ((ch == 'y') || (ch == 'Y'));
+}
+
