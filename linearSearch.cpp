37066fa8e416ee9b2e4262325ac6ffa082d199d3
RizkyPratama05 <rizkypp428@gmail.com>
2024-04-04 21:03:23 +0700

    menambahkan input

--- linearSearch.cpp
+++ linearSearch.cpp
@@ -6,3 +6,26 @@ int arr[20]; // Array to be searched
 int n;       // Number of element in the array
 int i;       // Index of array element
 
+void input ()
+{
+    while (true)
+    {
+        cout << "Enter the number of element in the array: ";
+        cin >> n;
+        if ((n > 0) && (n <= 20))
+            break;
+        else
+            cout << "\nArray should have minimum 1 and maximum 20 elements. \n\n";
+    }
+
+    // Accept array element
+    cout << "\n-------------------\n";
+    cout << " Enter array element \n";
+    cout << "---------------------\n";
+    for (i = 0; i < n; i++)
+    {
+        cout << "<" << (i + 1) << ">";
+        cin >> arr[i];     
+    }
+}
+
